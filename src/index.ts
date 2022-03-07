f0c066b2b199696c4c57b769e0c072ac519eaf1c
voomp <bat7gamer7@gmail.com>
2022-03-07 07:11:07 -0500

    getting full year in date

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,10 @@ client.on('ready', () => console.log('bot initialized'));
 const parseDateFormat = (date: string): string => {
 	const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 	const deltaDay = ['today', 'tomorrow'];
+	const currDate = new Date();
 
 	if(/^[a-zA-Z()]+$/.test(date)) {
 		if(!(days.concat(deltaDay)).includes(date)) return ''; // error handling
-		const currDate = new Date();
 
 		// delta time formula = 86400000*((days.indexOf(date)-currDate.getDay()));
 		let dateDelta = 0;
@@ -34,11 +34,11 @@ const parseDateFormat = (date: string): string => {
 		}
 		if(deltaDay.includes(date)) dateDelta = deltaDay.indexOf(date);
 
-		return `2022/${currDate.getMonth()+1}/${currDate.getDate()+dateDelta}`; // returning calculated data in date format
+		return `${currDate.getFullYear()}/${currDate.getMonth()+1}/${currDate.getDate()+dateDelta}`; // returning calculated data in date format
 	}
 
 	const dateParams = date.split('/');
-	return `2022/${dateParams[0]}/${dateParams[1]}`; // returning dateParams data in date format
+	return `${currDate.getFullYear()}/${dateParams[0]}/${dateParams[1]}`; // returning dateParams data in date format
 };
 
 const convertTime = (timeStr: string) => {
@@ -59,6 +59,7 @@ const parseTimeFormat = (format: string): string => {
 
 const getDateString = (format: string): string => {
 	const [date, time] = format.toUpperCase().split(' ');
+	
 }
 
 client.on('messageCreate', message => {
